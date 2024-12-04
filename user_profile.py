d389645e415d1afbf1dc22726d0ab3dc5b5ac180
Anj4li-Mishr4 <amishra12@gitam.in>
2024-12-04 03:40:57 +0000

    Version 2: Replace hardcoded variables with keyboard input

--- user_profile.py
+++ user_profile.py
@@ -1,7 +1,7 @@
-# Version 1: Hardcoded Variables
-            name = "Anjali Mishra"
-            age = 20
-            email = "amishra12@gitam.in"
+# Version 2: Using keyboard input for user details
+            name = input("Enter your name: ")
+            age = input("Enter your age: ")
+            email = input("Enter your email: ")
 
             print(f"User Profile Initialized: Name = {name}, Age = {age}, Email = {email}")
         
