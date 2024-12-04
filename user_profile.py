94c0c1f8dfed6d66d256d6d7664d922568fe550d
Anj4li-Mishr4 <amishra12@gitam.in>
2024-12-04 04:00:40 +0000

    Version 3: Read from file (single input)

--- user_profile.py
+++ user_profile.py
@@ -1,13 +1,11 @@
-#Version 3: Using a file for user details
-# Read name from a file
-with open("user_name.txt", "r") as file:
-    name = file.read().strip()
+# Version 4: Read From File (Multiple Inputs)
 
-# Other predefined values
-age = 21
-email = "amishra12@gitam.in"
-country = "India"
+# Read data from file
+with open("user_profile.txt", "r") as file:
+    name = file.readline().strip()    # First line is name
+    age = int(file.readline().strip())  # Second line is age
+    email = file.readline().strip()    # Third line is email
+    country = file.readline().strip()  # Fourth line is country
 
 # Output
 print(f"User Profile Initialized: Name = {name}, Age = {age}, Email = {email}, Country = {country}")
-        
