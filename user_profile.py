e72c11728e412691265f03ccfaf0d3e1f4663a1b
Anj4li-Mishr4 <amishra12@gitam.in>
2024-12-04 03:14:34 +0000

    Add user_profile.py with hardcoded variables

---
+++ user_profile.py
@@ -0,0 +1,7 @@
+# Version 1: Hardcoded Variables
+            name = "Anjali Mishra"
+            age = 20
+            email = "amishra12@gitam.in"
+
+            print(f"User Profile Initialized: Name = {name}, Age = {age}, Email = {email}")
+        
